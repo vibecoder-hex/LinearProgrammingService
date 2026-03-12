61e291e0876b8b77cb8c5c873e738e6d6c69a376
Семён Бабушкин <rty.sem@yandex.ru>
2026-03-12 17:23:02 +0300

    Added reverse normalization for simplex table printing

--- SimplexMethod/SimplexMethodCLI/Program.cs
+++ SimplexMethod/SimplexMethodCLI/Program.cs
@@ -306,7 +306,9 @@
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    Console.Write($"{_simplexTable[i][j].UpperBound, 2:F3}|{_simplexTable[i][j].LowerBound:F3} ");
+                    double upperBound = _simplexTable[i][j].UpperBound * _normalizationScale;
+                    double lowerBound = _simplexTable[i][j].LowerBound * _normalizationScale;
+                    Console.Write($"{upperBound, 2:F3}|{lowerBound:F3} ");
                 }
                 Console.WriteLine();
             }
